259723cecbf90d9ea2274a4aa5beb0ac8ad9f8f5
justincarothers <justincarothers@gmail.com>
2016-06-17 13:16:38 -0700

    worked on backend and finishing up front end

--- js/main.js
+++ js/main.js
@@ -1,8 +1,32 @@
-function PizzaMenu() { //constructor for pizza  menu
-
-  this.sizes = ["Baby Cheesus", "Medium", "Cheesus"];
-  this.sizeCost= [15, 20 30];
-  this.toppings = ["Olives", "Pepperoni", "Chicken", "Beef", ""]
-
-
+function Pizza() {
+  this.sizes = ['small', 'medium', 'large'];
+  this.sizesCost = [12, 16, 20];
+  this.toppings = ['pepperoni', 'chicken', 'mushroom', 'olives']
+  this.toppingsCost = 1;
+  this.pCost = 0;
 }
+Pizza.prototype.costOfPizza = function(size, howManyToppings) {
+  debugger;
+  if (this.sizes.indexOf(size) >= 0) {
+    this.pCost += this.sizesCost[this.sizes.indexOf(size)]
+  }
+  if (howManyToppings > 0) {
+    this.pCost += this.toppingsCost * howManyToppings;
+  }
+  return this.pCost;
+}
+newPizza = new Pizza();
+
+$(document).ready(function() {
+
+  var size = "";
+  var numOfToppings = 0;
+
+  $("#submit").click(function() {
+
+    if ($("#mushrooms").is(':checked'))
+    alert("YES!!");
+
+  });
+
+});
