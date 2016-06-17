8b84a0d491a45f3b7a242543d799766f41aee26b
justincarothers <justincarothers@gmail.com>
2016-06-17 08:20:25 -0700

    first commit getting started with the pizza project.

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function PizzaMenu() { //constructor for pizza  menu
+
+  this.sizes = ["Baby Cheesus", "Medium", "Cheesus"];
+  this.sizeCost= [15, 20 30];
+  this.toppings = ["Olives", "Pepperoni", "Chicken", "Beef", ""]
+
+
+}
